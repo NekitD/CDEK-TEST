d7c843e1d4b4d7dcc969ad62ad20642e50ed9b0a
Дорогин Никита Сергеевич <nikitadorogin@yandex.ru>
2026-04-14 17:43:11 +0700

    NewsElem first draft

---
+++ FRONT/news/src/component/newselem.jsx
@@ -0,0 +1,15 @@
+import Date from 'react'
+
+export default function NewsElem(props){
+    return (
+        <div className="NewsElem">
+            <img>{props.pic}</img>
+            <Date>{props.publishedAt}</Date>
+            <h2>{props.title}</h2>
+            <div>{props.rubrics}</div>
+            <div>{props.pop}</div>
+            <text>{props.likeCount}</text>
+            <text>{props.viewCount}</text>
+        </div>
+    );
+}
