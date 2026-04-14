1a518770962510d3144b030f197c04d0332e23ae
Дорогин Никита Сергеевич <nikitadorogin@yandex.ru>
2026-04-14 16:31:48 +0700

    don't report webVitals

--- FRONT/news/src/component/news.jsx
+++ FRONT/news/src/component/news.jsx
@@ -2,6 +2,7 @@ import fetcher from "./datafetcher";
 
 export default function NewsCompoonent(){
     let data = fetcher();
+    console.log("TEST API EXISTS:\n");
     console.log(data);
     return (
         <div className="News">
