52942d48dbedb2b1fce5ccc7ea61b917987e0d13
Дорогин Никита Сергеевич <nikitadorogin@yandex.ru>
2026-04-14 18:45:20 +0700

    map elems

--- FRONT/news/src/component/news.jsx
+++ FRONT/news/src/component/news.jsx
@@ -1,12 +1,38 @@
-import fetcher from "./datafetcher";
+import { useEffect, useState } from 'react'
+import Elem from './newselem';
+
 
 export default function NewsComponent(){
-    let data = fetcher();
-    console.log("TEST API EXISTS:\n");
-    console.log(data);
+    let [newsData, newsState] = useState([]);
+    let [pageNum, numState] = useState(1);
+    let [pageCount, countState] = useState(0);
+    
+    useEffect(()=>{
+        async function GetNews(page){
+            const promise = fetch(`http://1e14c3489fcb.vps.myjino.ru:5000/api/v1/news/feed/company/short?perPage=3&page=${page}`);
+            const answer = await promise;
+            const data = await answer.json();
+            console.log(data);
+            newsState(data.news);
+            countState(data.totalPages);
+            numState(page);
+        }
+        GetNews(pageNum);
+    }, []);
+
     return (
         <div className="News">
-        
+            {newsData.map((content, index)=>{
+                <Elem key={index}
+                    pic={content.pic}
+                    dat={content.publishedAt}
+                    txt={content.tittle}
+                    tags={content.rubrics}
+                    pop={content.pop}
+                    likeCount={content.likeCount}
+                    viewCount={content.viewCount}
+                />
+            })}
         </div>
     );
 }
