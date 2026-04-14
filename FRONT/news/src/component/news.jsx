8527ea9e2b02a6acb212afc381948d9d38308e30
Дорогин Никита Сергеевич <nikitadorogin@yandex.ru>
2026-04-14 21:30:45 +0700

    Add head and bottom for main component

--- FRONT/news/src/component/news.jsx
+++ FRONT/news/src/component/news.jsx
@@ -9,6 +9,29 @@ async function GetNews(setter, page){
     setter(data.news);
 }
 
+function convertDate(date) {
+    const dateObj = new Date();
+    const month = dateObj.getMonth() + 1;
+    let mstr = "";
+    switch (month)
+    {
+        case 1: mstr = "Январь"; break;
+        case 2: mstr = "Февраль"; break;
+        case 3: mstr = "Март"; break;
+        case 4: mstr = "Апрель"; break;
+        case 5: mstr = "Май"; break;
+        case 6: mstr = "Июнь"; break;
+        case 7: mstr = "Июль"; break;
+        case 8: mstr = "Август"; break;
+        case 9: mstr = "Сентябрь"; break;
+        case 10: mstr = "Октябрь"; break;
+        case 11: mstr = "Ноябрь"; break;
+        case 12: mstr = "Декабрь"; break;
+    }
+    const year = dateObj.getFullYear();
+    return mstr + " " + year;
+}
+
 export default function NewsComponent(props){
     
     let [newsData, setNews] = useState([]);
@@ -20,16 +43,27 @@ export default function NewsComponent(props){
 
     return (
         <div className="News">
-            {newsData.map((content, index)=>{
-                <Elem key={index}
-                    pic={content.pic}
-                    dat={content.publishedAt}
-                    title={content.title}
-                    rubrics={content.rubrics}
-                    likeCount={content.likeCount}
-                    viewCount={content.viewCount}
-                />
-            })}
+            <div className='head'>
+                <h1>Новости</h1>
+                <h3>{convertDate(newsData.minDatePublication)}</h3>
+            </div>
+            <hr className='headBorder'></hr>
+            <div className='content'>
+                {newsData.map((content, index)=>{
+                    <Elem key={index}
+                        pic={content.pic}
+                        dat={content.publishedAt}
+                        title={content.title}
+                        rubrics={content.rubrics}
+                        likeCount={content.likeCount}
+                        viewCount={content.viewCount}
+                    />
+                })}
+            </div>
+            <div className='bottom'>
+                {(pageNum > 1) ? <button onClick={pageNum -= 1}>←</button> : null}
+                {(pageNum < newsData.totalPages) ? <button onClick={pageNum += 1}>→</button> : null}
+            </div>
         </div>
     );
 }
