349d2803ae15373f173a4e988a9e104e09691789
Дорогин Никита Сергеевич <nikitadorogin@yandex.ru>
2026-04-15 13:09:54 +0700

    Add loadingStates

--- news/src/component/News.jsx
+++ news/src/component/News.jsx
@@ -3,13 +3,14 @@ import Elem from './newselem';
 import './News.css'
 import Loader from './skeleton'
 
-async function GetNews(msetter, nsetter, page){
-    nsetter([]);
+async function GetNews(msetter, nsetter, lsetter, lmsetter, page){
     const promise = fetch(`http://1e14c3489fcb.vps.myjino.ru:5000/api/v1/news/feed/company/short?perPage=3&page=${page}`);
     const answer = await promise;
     const data = await answer.json();
     msetter(data);
     nsetter(data.news);
+    lsetter(data.news.length === 0);
+    lmsetter(data.length === 0);
 }
 
 function convertMonthYear() {
@@ -52,16 +53,20 @@ export default function NewsComponent(props){
     let [metaData, setMeta] = useState(0)
     let [newsData, setNews] = useState([]);
     let [pageNum, setPage] = useState(1);
+    let [loading, setLoad] = useState(true);
+    let [mloading, setmLoad] = useState(true);
 
     useEffect(()=>{
-        GetNews(setMeta, setNews, pageNum);
-    }, [metaData, newsData]);
-    if (newsData.length === 0) {
+        GetNews(setMeta, setNews, setLoad, setmLoad, pageNum);
+    }, [metaData, newsData, loading. mloading]);
+    
+    if (mloading) {
         return (
         <div className="News">
             <Loader/>
         </div>);
     }
+
     return (
         <div className="News">
             <div className='Head'>
@@ -70,24 +75,30 @@ export default function NewsComponent(props){
             </div>
             <hr className='headBorder'></hr>
             <div className='Content'> 
-                {newsData.map((content, index)=>(
-                    <Elem key={index}
-                        pic={content.cover.images.hd}
-                        publishedAt={convertDate(content.publishedAt)}
-                        title={content.title}
-                        rubrics={content.rubrics}
-                        likeCount={content.likeCount}
-                        viewCount={content.viewCount}
-                    />
-                ))}
+                {
+                    (!loading) ?
+                        newsData.map((content, index)=>(
+                        <Elem key={index}
+                            pic={content.cover.images.hd}
+                            publishedAt={convertDate(content.publishedAt)}
+                            title={content.title}
+                            rubrics={content.rubrics}
+                            likeCount={content.likeCount}
+                            viewCount={content.viewCount}
+                        />
+                    )) : (<Loader/>)}
+
             </div>
             <div className='Bottom'>
                 <div className='bfields'>
                     <div className='pbfield'>
-                        {(pageNum > 1) ? <button className="pageback" onClick={()=>{setPage(pageNum - 1)}}>←</button> : null}
+                        {(pageNum > 1) ? 
+                        <button className="pageback" onClick={
+                            ()=>{setPage(pageNum - 1);}}>←</button> : null}
                     </div>
                     <div className='pffield'>
-                        {(pageNum < metaData.totalPages) ? <button className="pagefront" onClick={()=>{setPage(pageNum + 1)}}>→</button> : null}
+                        {(pageNum < metaData.totalPages) ? <button className="pagefront" onClick={
+                            ()=>{setPage(pageNum + 1);}}>→</button> : null}
                     </div>
                 </div>
             </div>
