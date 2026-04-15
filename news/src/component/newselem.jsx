7a75afe02858544b9cc710606cca00bf550ef062
Дорогин Никита Сергеевич <nikitadorogin@yandex.ru>
2026-04-15 14:05:56 +0700

    Rename newselem componeent blocks for BEM

--- news/src/component/newselem.jsx
+++ news/src/component/newselem.jsx
@@ -6,27 +6,27 @@ export default function NewsElem(props){
 
     if(props === null){
         return(
-            <div className="NewsElem">
+            <div className="news-elem">
                 <Loader />
             </div>
         );
     }
 
     return (
-        <div className="NewsElem">
-            <img className='elemPic' src={props.pic}/>
-            <div className='elemContent'>
-                <div className='elemConMain'>
-                    <div className='pubDate'>{props.publishedAt}</div>
-                    <h2 className='title'>{props.title}</h2>
+        <div className="news-elem">
+            <img className='news-elem__image' src={props.pic}/>
+            <div className='news-elem__content'>
+                <div className='news-elem__main'>
+                    <div className='news-elem__date'>{props.publishedAt}</div>
+                    <h2 className='news-elem__title'>{props.title}</h2>
                 </div>
-                <div className='elemConBottom'>
-                    <div className='elemRubrics'>props.rubrics</div>
-                    <div className='elemPopInfo'>
-                        <button className='likeButton'/>
-                        <text className='likeNum'>{props.likeCount}</text>
-                        <div className='viewIcon'/>
-                        <text className='viewNum'>{props.viewCount}</text>
+                <div className='news-elem__footer'>
+                    <div className='news-elem__rubrics'>props.rubrics</div>
+                    <div className='news-elem__stats'>
+                        <button className='news-elem__like-btn'/>
+                        <text className='news-elem__like-count'>{props.likeCount}</text>
+                        <div className='news-elem__view-icon'/>
+                        <text className='news-elem__view-count'>{props.viewCount}</text>
                     </div>
                 </div>
             </div>
